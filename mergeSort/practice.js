9bd821f30355cff9edaffa34fac78ac422c440dc
bebopvinh <vinhdo07@gmail.com>
2019-12-06 23:35:11 -0800

    modified mergeArrays algorithm

--- mergeSort/practice.js
+++ mergeSort/practice.js
@@ -1,22 +1,13 @@
 const a = [2, 4, 5, 7, 9]
 const b = [1, 3, 6, 8]
+const c = [45, 22, 8, 16, 5, 1, 4]
 
 function mergeArrays(arr1, arr2) {
     let [i, j] = [0, 0]
     let ans = []
-    while (true) {
+    while (i < arr1.length && j < arr2.length) {
         let first = arr1[i]
         let second = arr2[j]
-
-        if (!first) {
-            ans.push(second)
-            break
-        }
-
-        if (!second) {
-            ans.push(first)
-            break
-        }
 
         if (first <= second) {
             ans.push(first)
@@ -26,6 +17,15 @@ function mergeArrays(arr1, arr2) {
             j++
         }
     }
+
+    while (i < arr1.length) {
+        ans.push(arr1[i])
+        i++
+    }
+    while (j < arr2.length) {
+        ans.push(arr2[j])
+        j++
+    }
     return ans
 }
 
@@ -33,4 +33,9 @@ function mergeSort(arr) {
     if (arr.length <= 1) return arr
 
     let mid = Math.floor(arr.length / 2)
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+    return mergeArrays(left, right)
 }
+
+console.log(mergeSort(c))
